4fb0c0cde0b5433b752590a91120c80714655423
Jimba Abdullahi Olakunle <jimbaabdullahi9@gmail.com>
2025-03-18 02:56:34 +0100

    feat: update JavaScript

--- DOM1/app.js
+++ DOM1/app.js
@@ -1,32 +1,55 @@
-const price = document.querySelectorAll('.unit-price');
-const quantity = document.querySelectorAll('.quantity');
+const prices = document.getElementsByClassName('unit-price');
+const quantities = document.querySelectorAll('.quantity');
 const add = document.querySelectorAll('.fa-plus-circle');
 const minus = document.querySelectorAll('.fa-minus-circle');
 const clearItem = document.querySelectorAll('.fa-trash-alt');
-const heart = document.querySelectorAll('.fa-heart')
+const heart = document.querySelectorAll('.fa-heart');
+const totalCost = document.querySelector('.total');
 
-// add.addEventListener('click', love);
-// minus.addEventListener('click', love);
-// clearItem.addEventListener('click', love);
-add.forEach(add_icon => {
-    add_icon.addEventListener('click', btn =>{
-        btn.target;
-        quantity.forEach(add_quantity => {
-            add_quantity.textContent += 1;
-        })
+
+
+function updateTotal(){
+    let totalPrice = 0;
+    document.querySelectorAll(".card").forEach((item, index) => {
+        const price = Number(prices[index].textContent.replace('$', ''));
+        const quantity = Number(quantities[index].textContent);
+        totalPrice += price * quantity;
+    });
+    totalCost.textContent = `${totalPrice} $`;
+}
+
+
+// increase quantity
+add.forEach((add_icon, index) => {
+    add_icon.addEventListener('click', () =>{
+        let quantity = Number(quantities[index].textContent);
+        quantities[index].textContent = quantity + 1;
+        updateTotal();
+    })
 })
 
-    
+minus.forEach((minus_icon, index) => {
+    minus_icon.addEventListener('click', () => {
+        let quantity = Number(quantities[index].textContent);
+        quantities[index].textContent = quantities[index].textContent > 0 ? quantity - 1 : 0;
+        updateTotal();
+    });
+});
 
+clearItem.forEach((add_icon, index) => {
+    add_icon.addEventListener('click', () =>{
+        const price = Number(prices[index].textContent.replace('$', ''));
+        const quantity = Number(quantities[index].textContent);
+        const cost = Number(totalCost.textContent.replace('$', ''));
+        let count = price * quantity;
+        let costs = cost - count;
+        totalCost.textContent = `${costs} $`;
+        add_icon.closest(".card-body1").remove();
+    });
+})
 
 heart.forEach(icon => {
     icon.addEventListener('click' , event =>{
         event.target.style.color = event.target.style.color === 'black' ? 'red' : 'black' ;
     });
 });
-
-console.log(heart);
-console.log(quantity);
-console.log(add);
-console.log(minus);
-console.log(price);
